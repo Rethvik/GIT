da5032bbb344d35163b12f07c5402e5137c6c6c7
rethvik sai <rethvikdoddapaneni3@gmail.com>
2024-10-25 16:00:06 +0530

    ADDED CALLING FUNCTION

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ def simple_function():
 
 
 simple_function()
+# calling siple function in another function
+
+
+def call_another_function():
+    return simple_function()
+
+
+call_another_function()
