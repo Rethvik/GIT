6a941da49bcd1387a3f276a11dac052c29db8f0e
rethvik sai <rethvikdoddapaneni3@gmail.com>
2024-10-29 17:15:53 +0530

    changes

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-def simple_function():
+def simple_function(p):
     print("simple_function")
 
 
-simple_function()
+simple_function(1)
 # calling siple function in another function
 
 
 def call_another_function():
     l = "string"
-    return simple_function()
+    return simple_function(1)
 
 
 print("THIS IS ADDED IN FEATURE BRANCH")
